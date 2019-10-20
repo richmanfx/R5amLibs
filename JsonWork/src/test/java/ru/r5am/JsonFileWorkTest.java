977cf33036e111fe4377c934b0ac2522ead540dc
Aleksandr Jashhuk <richmanfx@yandex.ru>
2019-10-20 13:12:22 +0300

    Опробован AssertJ.

--- JsonWork/src/test/java/ru/r5am/JsonFileWorkTest.java
+++ JsonWork/src/test/java/ru/r5am/JsonFileWorkTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.apache.logging.log4j.LogManager;
+import static org.assertj.core.api.Assertions.*;
 
 class JsonFileWorkTest {
 
@@ -21,11 +22,9 @@ class JsonFileWorkTest {
         Map<String, Map<String, String>> data = jsonFileWork.readMapStringMap(jsonFileName);
         log.debug("Json MAP:\n{}", data);
 
-        assert data.toString()
-               .equals(
-                       "{Таблица 1={Поле 1=Значение 11, Поле 2=Значение 12, Поле 3=Значение 13, Поле 4=Значение 14, " +
-                       "Поле 5=Значение 15}, Таблица 2={Поле 1=Значение 21, Поле 2=Значение 22, Поле 3=Значение 23, " +
-                       "Поле 4=Значение 24, Поле 5=Значение 25}}");
+        assertThat(data).isEqualTo("{Таблица 1={Поле 1=Значение 11, Поле 2=Значение 12, Поле 3=Значение 13, Поле 4=Значение 14, " +
+                "Поле 5=Значение 15}, Таблица 2={Поле 1=Значение 21, Поле 2=Значение 22, Поле 3=Значение 23, " +
+                "Поле 4=Значение 24, Поле 5=Значение 25}}");
     }
 
 }
