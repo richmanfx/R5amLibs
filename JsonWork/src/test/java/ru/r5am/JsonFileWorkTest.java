a642f4223122a29fabf7d6b9abc3f06256a26fc2
Aleksandr Jashhuk <richmanfx@yandex.ru>
2019-10-20 13:35:23 +0300

    Опробован AssertJ.

--- JsonWork/src/test/java/ru/r5am/JsonFileWorkTest.java
+++ JsonWork/src/test/java/ru/r5am/JsonFileWorkTest.java
@@ -22,9 +22,15 @@ class JsonFileWorkTest {
         Map<String, Map<String, String>> data = jsonFileWork.readMapStringMap(jsonFileName);
         log.debug("Json MAP:\n{}", data);
 
-        assertThat(data).isEqualTo("{Таблица 1={Поле 1=Значение 11, Поле 2=Значение 12, Поле 3=Значение 13, Поле 4=Значение 14, " +
+        assertThat(data).hasSize(2);
+
+        assert data.toString().equals(
+                "{Таблица 1={Поле 1=Значение 11, Поле 2=Значение 12, Поле 3=Значение 13, Поле 4=Значение 14, " +
                 "Поле 5=Значение 15}, Таблица 2={Поле 1=Значение 21, Поле 2=Значение 22, Поле 3=Значение 23, " +
                 "Поле 4=Значение 24, Поле 5=Значение 25}}");
+
+
+
     }
 
 }
--- YamlWork/src/test/java/ru/r5am/YamlFileWorkTest.java
+++ YamlWork/src/test/java/ru/r5am/YamlFileWorkTest.java
@@ -1,12 +1,12 @@
 package ru.r5am;
 
-import org.apache.logging.log4j.LogManager;
+import java.util.Map;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import static org.assertj.core.api.Assertions.*;
 
 class YamlFileWorkTest {
 
@@ -21,10 +21,11 @@ class YamlFileWorkTest {
         Map<String, Map<String, String>> data = yamlFileWork.readMapStringMap(yamlFileName);
         log.debug("Yaml MAP:\n{}", data);
 
-        assert data.toString()
-                .equals(
-                        "{Таблица 1={Поле 1=Значение 11, Поле 2=Значение 12, Поле 3=Значение 13, Поле 4=Значение 14, " +
-                        "Поле 5=Значение 15}, Таблица 2={Поле 1=Значение 21, Поле 2=Значение 22, Поле 3=Значение 23, " +
-                        "Поле 4=Значение 24, Поле 5=Значение 25}}");
+        assertThat(data).hasSize(2);
+
+        assert data.toString().equals(
+                "{Таблица 1={Поле 1=Значение 11, Поле 2=Значение 12, Поле 3=Значение 13, Поле 4=Значение 14, " +
+                "Поле 5=Значение 15}, Таблица 2={Поле 1=Значение 21, Поле 2=Значение 22, Поле 3=Значение 23, " +
+                "Поле 4=Значение 24, Поле 5=Значение 25}}");
     }
 }
